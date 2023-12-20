fab40dd7cd0ffedca5ea14dbfc66c3e668c63efc
Dima Enns <dieter.enns@gmail.com>
2023-12-20 23:25:53 +0100

    First prototype sketch

---
+++ RocksIsolationPrototype/PrototypicalIsolation.cs
@@ -0,0 +1,89 @@
+﻿using Rocks;
+using Rocks.Expectations;
+
+namespace RocksIsolationPrototype;
+
+public static class RockProto 
+{
+    public static SubjectIsolation Isolate<T>() where T : class
+    {
+        if (typeof(T) == typeof(Subject))
+        {
+            return new SubjectIsolation();
+        }
+        throw new Exception("This should never happen");
+    }
+}
+
+public class SubjectIsolation
+{
+    private readonly Lazy<IsoProperties> _properties;
+    private readonly Lazy<IsoConstructorParameters> _constructorParameters;
+    
+    private readonly Lazy<Expectations<INeedsToBeMeaningOfEverything>> _prop_meaningOfEverything = new(() => Rock.Create<INeedsToBeMeaningOfEverything>());
+    private readonly Lazy<Expectations<IIrrelevant0>> _prop_irrelevant0 = new(() => Rock.Create<IIrrelevant0>());
+    private readonly Lazy<Expectations<PessimisticComponent>> _constr_pessimisticComponent = new(() => Rock.Create<PessimisticComponent>());
+    private readonly Lazy<Expectations<IIrrelevant1>> _constr_irrelevant1 = new(() => Rock.Create<IIrrelevant1>());
+    
+    public SubjectIsolation()
+    {
+        _properties = new Lazy<IsoProperties>(() => new IsoProperties(this));
+        _constructorParameters = new Lazy<IsoConstructorParameters>(() => new IsoConstructorParameters(this));
+    }
+    
+    public IsoProperties Properties() => _properties.Value;
+    public IsoConstructorParameters ConstructorParameters() => _constructorParameters.Value;
+    
+    public Subject Instance(MockMeIfYouCan mockMeIfYouCan) => 
+        new(_constr_pessimisticComponent.IsValueCreated ? _constr_pessimisticComponent.Value.Instance() : Rock.Make<PessimisticComponent>().Instance(), 
+            _constr_irrelevant1.IsValueCreated ? _constr_irrelevant1.Value.Instance() : Rock.Make<IIrrelevant1>().Instance(),
+            mockMeIfYouCan)
+        {
+            MeaningOfEverything = _prop_meaningOfEverything.IsValueCreated ? _prop_meaningOfEverything.Value.Instance() : Rock.Make<INeedsToBeMeaningOfEverything>().Instance(),
+            Irrelevant0 = _prop_irrelevant0.IsValueCreated ? _prop_irrelevant0.Value.Instance() : Rock.Make<IIrrelevant0>().Instance()
+        };
+    
+    public void Verify()
+    {
+        if (_prop_meaningOfEverything.IsValueCreated)
+        {
+            _prop_meaningOfEverything.Value.Verify();
+        }
+        if (_prop_irrelevant0.IsValueCreated)
+        {
+            _prop_irrelevant0.Value.Verify();
+        }
+        if (_constr_pessimisticComponent.IsValueCreated)
+        {
+            _constr_pessimisticComponent.Value.Verify();
+        }
+        if (_constr_irrelevant1.IsValueCreated)
+        {
+            _constr_irrelevant1.Value.Verify();
+        }
+    }
+
+    public class IsoProperties
+    {
+        private readonly SubjectIsolation _subjectIsolation;
+        public Expectations<INeedsToBeMeaningOfEverything> MeaningOfEverything => _subjectIsolation._prop_meaningOfEverything.Value;
+        public Expectations<IIrrelevant0> Irrelevant0 => _subjectIsolation._prop_irrelevant0.Value;
+        
+        public IsoProperties(SubjectIsolation subjectIsolation)
+        {
+            _subjectIsolation = subjectIsolation;
+        }
+    }
+    
+    public class IsoConstructorParameters
+    {
+        private readonly SubjectIsolation _subjectIsolation;
+        public Expectations<PessimisticComponent> PessimisticComponent => _subjectIsolation._constr_pessimisticComponent.Value;
+        public Expectations<IIrrelevant1> Irrelevant1 => _subjectIsolation._constr_irrelevant1.Value;
+        
+        public IsoConstructorParameters(SubjectIsolation subjectIsolation)
+        {
+            _subjectIsolation = subjectIsolation;
+        }
+    }
+}
---
+++ RocksIsolationPrototype/Test.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+
+namespace RocksIsolationPrototype;
+
+public interface INeedsToBeMeaningOfEverything
+{
+    int Value { get; }
+}
+
+public enum GlassState
+{
+    HalfFull,
+    HalfEmpty
+}
+
+public class PessimisticComponent
+{
+    public virtual GlassState GetState() => GlassState.HalfEmpty;
+}
+
+public class MockMeIfYouCan
+{
+    public int Unmockable() => -42;
+}
+
+public interface IIrrelevant0
+{
+    void Void();
+}
+
+public interface IIrrelevant1
+{
+    int HamsterTreadmillRounds { get; }
+}
+
+public class Subject
+{
+    private readonly PessimisticComponent _pessimisticComponent;
+    public required INeedsToBeMeaningOfEverything MeaningOfEverything { get; init; }
+    public required IIrrelevant0 Irrelevant0 { get; init; }
+    
+    public Subject(PessimisticComponent pessimisticComponent, IIrrelevant1 irrelevant1, MockMeIfYouCan mockMeIfYouCan)
+    {
+        _pessimisticComponent = pessimisticComponent;
+    }
+
+    public bool ToBeTested() => _pessimisticComponent.GetState() == GlassState.HalfFull && MeaningOfEverything.Value == 42;
+}
+
+public static class Test
+{
+    public static void Run()
+    {
+        /* This is the code necessary for expectation management without isolation
+        var meaningExpectations = Rock.Create<INeedsToBeMeaningOfEverything>();
+        meaningExpectations.Properties().Getters().Value().Returns(42);
+        var pessimisticComponentExpectations = Rock.Create<PessimisticComponent>();
+        pessimisticComponentExpectations.Methods().GetState().Returns(GlassState.HalfFull);
+        var irrelevant0Make = Rock.Make<IIrrelevant0>();
+        var irrelevant1Make = Rock.Make<IIrrelevant1>();
+
+        var subject = new Subject(pessimisticComponentExpectations.Instance(), irrelevant1Make.Instance(), new MockMeIfYouCan())
+        {
+            MeaningOfEverything = meaningExpectations.Instance(),
+            Irrelevant0 = irrelevant0Make.Instance()
+        };
+        */
+
+        var subjectIsolation = RockProto.Isolate<Subject>();
+        subjectIsolation.Properties().MeaningOfEverything.Properties().Getters().Value().Returns(42);
+        subjectIsolation.ConstructorParameters().PessimisticComponent.Methods().GetState().Returns(GlassState.HalfFull);
+        
+        var subject = subjectIsolation.Instance(new MockMeIfYouCan());
+
+        var result = subject.ToBeTested();
+
+        Debug.Assert(result);
+        
+        subjectIsolation.Verify();
+        
+        /* Also the verification without isolation
+        meaningExpectations.Verify();
+        pessimisticComponentExpectations.Verify();
+        */
+    }
+}
